dc419a4a791be073b14c54fa9f01634e1f87e80c
LMezza <mezabalza18@gmail.com>
2024-12-08 13:10:46 +0000

    Correction of ColorThemeTogglerNavbar

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-black">
+    <nav className="navbar navbar-expand-lg bg-black" data-bs-theme="dark">
       <div className="container-fluid">
         <a className="navbar-brand text-white" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end text-white" id="Start Bootstrap">
