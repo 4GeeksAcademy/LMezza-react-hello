e0a576363a7169d8c3865fed1d4f33733f820573
LMezza <mezabalza18@gmail.com>
2024-12-08 13:21:49 +0000

    Correction of id Collapse Navbar

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -4,11 +4,11 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-black" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand text-white" href="#">Navbar</a>
+        <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end text-white" id="Start Bootstrap">
+        <div className="collapse navbar-collapse justify-content-end text-white" id="navbarSupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
